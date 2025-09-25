f9a63bac72faac139cb448795752c744ee456441
Alexandre Reyes Mengual <24470180@campeche.tecnm.mx>
2025-09-24 21:06:58 -0600

    elemine archivos innecesarios

--- topicos avanzados/ContadorManual/ContadorManual.Gui/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ContadorManual.Gui")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ContadorManual.Gui.Pages")]
